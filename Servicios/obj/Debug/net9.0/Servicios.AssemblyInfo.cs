4670afbdba1f6abda7b5393e777214a64de438d1
GrassiOS <148152704+GrassiOS@users.noreply.github.com>
2025-06-05 11:47:29 -0700

    GOAT

--- Servicios/obj/Debug/net9.0/Servicios.AssemblyInfo.cs
+++ Servicios/obj/Debug/net9.0/Servicios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Servicios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aff411bbc91b12984bfa723c7b8a8844b3b887d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c834089404afb9ba114c711cf8bfcf66ca2aac3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Servicios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Servicios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
